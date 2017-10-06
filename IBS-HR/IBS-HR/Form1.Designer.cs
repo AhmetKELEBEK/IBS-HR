fa7b0318e8d0cb5b476df9d89f9f9f56cb2d8c47
Ahmet KELEBEK <30650016+AhmetKELEBEK@users.noreply.github.com>
2017-10-06 09:30:23 +0300

    IBS-HR
    Kullanıcı Giriş Ekranı

--- IBS-HR/IBS-HR/Form1.Designer.cs
+++ IBS-HR/IBS-HR/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(164, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(178, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(486, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::IBS_HR.Properties.Resources.Başlıksız_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(523, 276);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- IBS-HR/IBS-HR/Form1.cs
+++ IBS-HR/IBS-HR/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,108 @@ namespace IBS_HR
 {
     public partial class Form1 : Form
     {
+        public object SplashScreenManager { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            button2.Visible = false;
+                try
+            {
+            label1.Text = "Veri Tabanı Araçları Kontrol Ediliyor.";
+            string VeritataniAdi = "ibs";
+            SqlConnection baglanti = new SqlConnection("server=.\\SQLEXPRESS;database=master; Integrated Security=SSPI");
+            
+            SqlCommand komut = new SqlCommand("SELECT Count(name) FROM master.dbo.sysdatabases WHERE name=@prmVeritabani", baglanti);
+            komut.Parameters.AddWithValue("@prmVeriTabani", VeritataniAdi);
+            
+            baglanti.Open();
+            
+            int sonuc = (int)komut.ExecuteScalar();
+                if (sonuc != 0)
+                {
+                    label1.Text = "";
+                    label1.Text = "Kontrol Başarılı, Giriş Yapılabilir";
+                }
+                else
+                {
+                    DialogResult dialogResult = MessageBox.Show("Programı İlk Defa Kullanıdığınız Tespit Edildi.\n Veri Tabanı Kurulumu Yapılsın mu ?", "İlk Açılış", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        komut.CommandText = "Create Database " + VeritataniAdi;
+                        komut.ExecuteNonQuery();
+                        using (SqlCommand command = new SqlCommand("CREATE TABLE Kisiler(Ad char(50),Soyad char(50),Adres char(50));", baglanti))
+                            baglanti.Close();
+                        
+                        string baglantiCumlesi = "server=.\\SQLEXPRESS; database=ibs; integrated security=SSPI";
+                        using (SqlConnection tolustur = new SqlConnection(baglantiCumlesi))
+                            try
+                            {
+
+                                tolustur.Open();
+                                using (SqlCommand command = new SqlCommand("CREATE TABLE Kisiler(Ad char(50),Soyad char(50),Adres char(50));", tolustur))
+
+                                   command.ExecuteNonQuery();
+
+                                
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Kurgu Hazırlanırken Hata." + ex.Message);
+                            }
+
+                        label1.Text = "";
+                        label1.Text = "İlk Kurulum Başarılı Oldu.";
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+                    
+
+                }
+                baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Yerel Sunucu İle Bağlantı Başarısız. \n" + ex.Message);
+                label1.Text = "Yerel Sunucu Bağlantısı Başarısız. Giriş Yapılamaz.";
+                button2.Visible = true;
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            yardım Form1 = new yardım();
+            this.Hide();
+            Form1.Show();
+        }
     }
-}
+    }
+
